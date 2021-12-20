8886800c6fb694d2070b2c603d5093033285e3cb
nadzul24 <nadnadzul@gmail.com>
2021-12-20 13:47:10 +0700

    add config

--- php/src/admin/includes/config.php
+++ php/src/admin/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER','localhost');
+define('DB_SERVER','172.20.0.2');
 define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_PASS' ,'admin');
 define('DB_NAME','newsportal');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
